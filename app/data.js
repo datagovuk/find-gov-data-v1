d3b724e385cd44970dad1536754c86678d301940
Ross Jones <ross@servercode.co.uk>
2017-04-18 13:54:09 +0100

    Add a container for some static data.

---
+++ app/data.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // Lists of organisations
+  organisations: [
+    {
+        title: "Cabinet Office",
+        name: "cabinet-office",
+        type: "national"
+    },
+    {
+        title: "Cheshire West and Chester",
+        name: "cheshire-west-and-chester",
+        type: "local"
+    },
+    {
+        title: "Wirral Council",
+        name: "wirral-council",
+        type: "local"
+    }
+  ]
+}
--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,13 @@
 var express = require('express')
 var router = express.Router()
+var data = require('./data.js')
+
+var app = express()
+
+router.use(function(req,res,next){
+  res.locals.data = data
+  next()
+})
 
 // Route index page
 router.get('/', function (req, res) {
